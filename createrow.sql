fc96664722c25c3c1b7ada11b2d5685a2cd29b2c
NochexDevTeam <36759878+NochexDevTeam@users.noreply.github.com>
2021-07-08 09:06:21 +0100

    Update createrow.sql

--- createrow.sql
+++ createrow.sql
@@ -1,13 +1,1 @@
-INSERT INTO jos_jshopping_payment_method
-(
-`name_en-GB`, `name_de-DE`, `payment_code`, `payment_class`, `payment_publish`, `payment_ordering`, `payment_params`, `payment_type`, `price`, `tax_id`, `show_descr_in_email`
-)
-VALUES
-(
-'Nochex', 'Nochex', 'nochex', 'pm_nochex', 1, 7, 'testmode=0
-email_received=
-transaction_end_status=6
-transaction_pending_status=1
-transaction_failed_status=3
-checkdatareturns=1', 2, 0.00, 1, 0
-)
+INSERT INTO jos_jshopping_payment_method ( `payment_id`,`payment_code`, `payment_class`,`scriptname`, `payment_publish`, `payment_ordering`, `payment_params`, `payment_type`, `price`, `price_type`,`tax_id`,`image`, `show_descr_in_email`,`order_description`,`name_en-GB`,`description_en-GB`, `name_de-DE`, `description_de-DE` ) VALUES ( 6,'nochex', 'pm_nochex', 'pm_nochex', 1, 7, 'testmode=0 email_received= transaction_end_status=6 transaction_pending_status=1 transaction_failed_status=3 checkdatareturns=1', 2, 0.00,2, 1, "", 0,"",'Nochex','', 'Nochex','' )
